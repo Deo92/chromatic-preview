5b104777a6c27686d9bea7962160ae9e7e5caae4
Deo Gochava <deo.gochava@gmail.com>
2022-11-07 17:50:54 +0400

    Update FormExample.tsx

--- src/FormExample/FormExample.tsx
+++ src/FormExample/FormExample.tsx
@@ -31,7 +31,7 @@ export const FormExample: React.FC = () => {
                             'data-testid': 'input-last',
                         }}
                     />
-                    <Typography>{`Welcome Here: ${firstName} ${lastName}`}</Typography>
+                    <Typography>{`Welcome: ${firstName} ${lastName}`}</Typography>
                 </Stack>
             </Paper>
         </Stack>
